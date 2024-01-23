b413dc29151bd89998df281698393855df76b7c1
Hod_wys <92233601+hodwys@users.noreply.github.com>
2024-01-23 15:40:23 +0200

    Add files via upload

---
+++ Assets/TimeSpawnerRandom.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/**
+ * This component instantiates a given prefab at random time intervals and random bias from its object position.
+ */
+public class TimedSpawnerRandom : MonoBehaviour
+{
+    [SerializeField] MoveEnemy prefabToSpawn;
+    [SerializeField] Vector3 velocityOfSpawnedObject;
+    [SerializeField] float Dis;
+    [Tooltip("Minimum time between consecutive spawns, in seconds")][SerializeField] float minTimeBetweenSpawns;
+    [Tooltip("Maximum time between consecutive spawns, in seconds")][SerializeField] float maxTimeBetweenSpawns;
+    [Tooltip("Maximum distance in X between spawner and spawned objects, in meters")][SerializeField] float maxXDistance;
+
+    void Start()
+    {
+        this.StartCoroutine(SpawnRoutine());    // co-routines
+    }
+
+    IEnumerator SpawnRoutine()
+    {    // co-routines
+        while (true)
+        {
+            float timeBetweenSpawnsInSeconds = Random.Range(minTimeBetweenSpawns, maxTimeBetweenSpawns);
+            yield return new WaitForSeconds(timeBetweenSpawnsInSeconds);       // co-routines
+            Vector3 positionOfSpawnedObject = new Vector3(
+                transform.position.x + Random.Range(-maxXDistance, +maxXDistance),
+                transform.position.y,
+                transform.position.z);
+            GameObject newObject = Instantiate(prefabToSpawn.gameObject, positionOfSpawnedObject, Quaternion.identity);
+            newObject.GetComponent<MoveEnemy>().SetVelocity(velocityOfSpawnedObject);
+        }
+    }
+
+    private void Update()
+    {
+       if (transform.position.y < Dis)
+        {
+            Destroy(gameObject);
+        } 
+    }
+
+
+}
